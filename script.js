df3ca751b043b96c3a82ee88d1c20a2c2d2fb6f7
brianmueller <brianscottmueller@gmail.com>
2021-11-20 15:10:54 -0500

    cleanup, minor change

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /*
 TODO
-- revamp summary
 - when 15 seconds have passed: notification to load newPosts: loadTop: newRandomPosts at top
 - add comment/share
 */
@@ -13,54 +12,6 @@ let userName;
 
 
 /********** WELCOME **********/
-
-
-
-
-/********** APP **********/
-
-// userPosts, friendPosts, newsPosts
-
-/*
-
-userPost :
-{
-    text: "lorem ipsum",
-    numLikes: 0
-}
-
-friendPost : 
-{
-    text: "lorem ipsum",
-    image: "imageURL",
-    numLikes: random
-}
-
-newsPost : 
-{
-    text: "lorem ipsum",
-    image: "imageURL",
-    numLikes: random
-}
-
-*/
-
-// newRandomPosts
-    // 5 new friendPosts, 5 new newsPosts
-// userPost at top
-// when scrolled to bottom:
-    // loadBottom: newRandomPosts at bottom
-// when 15 seconds have passed: notification to load newPosts
-    // loadTop: newRandomPosts at top
-
-
-
-let allPosts = [];
-// let userPosts = [];
-// let friendPosts = [];
-// let newsPosts = [];
-
-let feed = document.querySelector('#feed');
 
 let categories = [
     "all",
@@ -83,7 +34,6 @@ String.prototype.capitalize = function() {
 function insertCategories(){
     let categoriesDom = document.createElement("div");
     categories.forEach(function(category){
-        // categoriesDom.innerHTML += `<input class="single-checkbox" type="checkbox" name="category" value="${category}"> ${category.capitalize()}<br>`;
         if(category == "all"){
             categoriesDom.innerHTML += `<input type="radio" name="category" checked="checked" value="${category}"> ${category.capitalize()}<br>`;
         } else {
@@ -91,32 +41,19 @@ function insertCategories(){
         }
             
     });
-    // document.querySelector("#welcome > .container").insertBefore(categoriesDom, document.querySelector("#start"));
     let parentElement = document.querySelector("#welcome > .container");
     parentElement.insertBefore(categoriesDom, parentElement.children[3]);
 
 }
 insertCategories();
 
-// checkboxes
-// var limit = 3;
-// $('input.single-checkbox').on('change', function(evt) {
-//    if($(this).siblings(':checked').length >= limit) {
-//        this.checked = false;
-//    }
-// });
-
-// function getUserCategories(){
-//     var userCategories = []
-//     var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
-
-//     for (var i = 0; i < checkboxes.length; i++) {
-//         userCategories.push(checkboxes[i].value)
-//     }
-//     return userCategories;
-// }
 
 
+/********** APP **********/
+
+
+let allPosts = [];
+let feed = document.querySelector('#feed');
 
 
 // USER POST
@@ -160,10 +97,6 @@ function shuffleArray(array) {
 let newsData = []; // to be filled with objects
 
 function populateNewsData(newsCategory){
-    // $.getJSON('https://inshortsapi.vercel.app/news?category=science', function(response) {
-    //     newsData.push.apply(newsData,response.data); // append new array to existing array
-    //     console.log(newsData);
-    // });
     return $.getJSON("https://inshortsapi.vercel.app/news?category="+newsCategory).then(function(response){
         return response.data;
     });
@@ -171,9 +104,6 @@ function populateNewsData(newsCategory){
 
 function generateNewsPost(){
     let newsArticle = newsData.splice(Math.floor(Math.random()*newsData.length),1)[0]; // returns an array, so gets first element
-    // console.log(newsArticle)
-    // <p>News ${Math.floor(Math.random()*10+1)}</p>
-    // <p>${newsArticle.title}</p>
     result = `
         <div class="${cardWidths} feed-card">
             <div class="card">
@@ -197,11 +127,6 @@ function populateFriendsCategoryPhotos(category){
     if(category == "all"){
         // merge all arrays
         for (const categoryI in allCategoryPhotos) {
-            // container.innerHTML += `<h1>${category}</h1>`;
-            // allCategoryPhotos[category].forEach(function(photo){
-            //     container.innerHTML += `<p>${photo.description}</p>`;
-            //     container.innerHTML += `<img src=${photo.url}>`;
-            // });
             friendsCategoryPhotos.push(allCategoryPhotos[categoryI]);
         }
         friendsCategoryPhotos = [].concat.apply([], friendsCategoryPhotos);
@@ -212,31 +137,10 @@ function populateFriendsCategoryPhotos(category){
 }
 
 function generateFriendPost(){
-    // result = `
-    //     <div class="col-sm-6 feed-card">
-    //         <p>Friend ${Math.floor(Math.random()*10+1)}</p>
-    //     </div>
-    // `;
-
-    // result = `
-    //     <div class="col-sm-6 feed-card">
-    //         <div class="card">
-    //             <div class="card-body">
-    //                 <h5 class="card-title">Name ${Math.floor(Math.random()*10+1)}</h5>
-    //                 <h6 class="card-subtitle mb-2 text-muted">Date</h6>
-    //                 <p class="card-text">Lorem ipsum...</p>
-    //                 <a href="#" class="card-link">Card link</a>
-    //                 <a href="#" class="card-link">Another link</a>
-    //             </div>
-    //         </div>
-    //     </div>
-    // `;
-
+    
     let friend = friends[Math.floor(Math.random()*100)];
     let photo = friendsCategoryPhotos.pop();
     
-    // <h6 class="card-subtitle mb-2 text-muted">Date</h6>
-
     result = `
         <div class="${cardWidths} feed-card">
             <div class="card">
@@ -253,8 +157,6 @@ function generateFriendPost(){
             </div>
         </div>
     `;
-
-
     return result;
 }
 
@@ -308,13 +210,6 @@ document.querySelector('#start').addEventListener('click',function(){
     let greeting = userName.length > 0 ? "Welcome, " + userName + "!" : "Welcome!";
     document.querySelector('#greeting').innerHTML = greeting;
 
-    // checkboxes
-    // let userCategories = getUserCategories();
-    // console.log(userCategories);
-    // if(userCategories.length >= 1){
-    //     newsCategory1 = userCategories[0];
-    // }
-
     // single radio
     let radios = document.getElementsByName("category");
     let selected = Array.from(radios).find(radio => radio.checked);
@@ -337,25 +232,6 @@ document.querySelector('#start').addEventListener('click',function(){
 
 
 function addInfiniteScrolling(){
-    // window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-
-    //     // check to load more
-    //     if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight) {
-    //         // you're at the bottom of the page
-    //         // feed.innerHTML += generateNewRandomPosts()
-    //         console.log("at the bottom" + Math.floor(Math.random()*10));
-    //     }
-    // });
-    
-    // document.addEventListener('scroll', function (event) {
-    //     console.log("scrollHeight: " + document.body.scrollHeight + "; scrollTop: " + document.body.scrollTop + "; innerHeight: " + window.innerHeight);
-    //     if (document.body.scrollHeight == 
-    //         document.body.scrollTop +        
-    //         window.innerHeight) {
-    //         alert("Bottom!");
-    //     }
-    // });
-    
     $(window).scroll(function() {
         if($(window).scrollTop() + $(window).height() == $(document).height()) {
             // alert("bottom!");
@@ -385,30 +261,8 @@ function addUserData(data){
     userDataDom.innerHTML += `<li class="list-group-item">${data}</li>`;
 }
 
-
-
 let summaryWrapper = document.querySelector('#summary');
 let summary = document.querySelector('#summary > .container');
-
-
-// let timeGuess = document.createElement('p');
-// summary.appendChild(timeGuess);
-
-// let timeSpent = document.createElement('p');
-// summary.appendChild(timeSpent);
-
-// let timeDifference = document.createElement('p');
-// summary.appendChild(timeDifference);
-
-// let userClicks = document.createElement('p');
-// summary.appendChild(userClicks);
-
-// let userDown = document.createElement('p');
-// summary.appendChild(userDown);
-
-// let userUp = document.createElement('p');
-// summary.appendChild(userUp);
-
 
 // https://ipdata.co/blog/how-to-get-the-ip-address-in-javascript/
 function json(url) {
@@ -425,7 +279,6 @@ json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
 function addS(count){
     return count == 1 ? "" : "s";
 }
-
 
 document.querySelector('#finish').addEventListener('click',function(){
     window.scrollTo(0, 0);
@@ -459,7 +312,6 @@ let scrollingDownRecently = false;
 let scrollDownCount = 0;
 let scrollUpCount = 0;
 
-// // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
 window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
 
     // count
